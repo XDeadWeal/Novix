ef4ba3d6c8dd33a50c6c0a4f33b8bc7e474d5b23
XDead <stegancovr@gmail.com>
2026-09-05 23:55:31 +0300

    Update keyboard.h

--- include/kernel/keyboard.h
+++ include/kernel/keyboard.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-define KEYBOARD_DATA_PORT 0x60
-define KEYBOARD_STATUS_PORT 0x64
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
 
 void keyboard_init();
 char keyboard_get_char();
